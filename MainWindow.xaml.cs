74cd887ff55be73fd5ce6ff720137c891e1a0b4e
Roger Gost <rogergost04@gmail.com>
2024-09-25 16:53:53 +0200

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExPractics01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckClick(object sender, RoutedEventArgs e)
+        {
+            boto1.Content = "Noches";
+        }
+        private void CheckUnclick(object sender, RoutedEventArgs e)
+        {
+            boto1.Content = "Buenas";
+        }
+
+        private void botton2(object sender, RoutedEventArgs e)
+        {
+            int numeros;
+            if (int.TryParse(Contador.Content.ToString(), out numeros)) {
+                if (check.IsChecked == true)
+                {
+                    numeros += 1;
+                    Contador.Content = numeros;
+                }
+            }
+                
+        }
+
+    }
+}
